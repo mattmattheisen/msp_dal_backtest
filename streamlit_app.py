38941b2e5c48065b6899b24964674bcc0641b45a
mattmattheisen <mattheisens@frontiernet.net>
2026-04-20 19:10:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ import matplotlib.gridspec as gridspec
 
 st.set_page_config(
     page_title="MSP–DAL Backtest | Shomer Analytics",
-    page_icon=NONE,
+    page_icon=None,
     layout="wide",
 )
 
